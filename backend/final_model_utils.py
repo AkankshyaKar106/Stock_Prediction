8caa635e978342c99b6110cfc9f72f29a6bb7a38
AkankshyaKar106 <162710994+AkankshyaKar106@users.noreply.github.com>
2025-04-21 12:52:36 +0530

    Update final_model_utils.py

--- backend/final_model_utils.py
+++ backend/final_model_utils.py
@@ -9,6 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from keras.metrics import MeanSquaredError
+import joblib
 import talib
 import pytz
 import sqlite3
