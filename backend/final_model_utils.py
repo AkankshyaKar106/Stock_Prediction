c2b04a2b7a7c11ff0e52164e0a6cacea2428f2ea
AkankshyaKar106 <162710994+AkankshyaKar106@users.noreply.github.com>
2025-04-22 12:52:03 +0530

    Update final_model_utils.py

--- backend/final_model_utils.py
+++ backend/final_model_utils.py
@@ -42,7 +42,7 @@ def load_lstm_model():
 def load_rf_model():
     try:
         rf_url = (
-            "https://drive.google.com/uc?id=1fJrGLRvSK14ygGDeSyYczE6utyj8Mqim")
+            https://drive.google.com/uc?id=1JTn2pKj1uebMI_cTt--pts1UToYv_D6K)
         temp_file = "temp_rf.pkl"  # Temporary file
 
         # Download the model temporarily
